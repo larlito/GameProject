a533eacc9a7643f58476a70ba0cd9a9af1cf626d
larlito <aligaziev04@mail.ru>
2024-05-08 20:23:55 +0300

    def commands

--- bl_upp.py
+++ bl_upp.py
@@ -1,8 +1,28 @@
 import bl_low
 
-def commands(command: str):
+def commands():
+    command = input('Введите команду которую хотите использовать >> ')
+    
     if command == 'create':
         result = bl_low.create_catalog()
 
-    if command == 'del catalog';
-        result = bl_low.
+    if command == 'delete catalog':
+        result = bl_low.delete_catalog()
+
+    if command == 'info':
+        result = bl_low.info_catalogs()
+
+    if command == 'book':
+        result = bl_low.add_book()
+
+    if command == 'read':
+        result = bl_low.read_catalog()
+
+    if command == 'find':
+        result = bl_low.find_book()
+
+    if command != 'create' and command != 'delete catalog' and command != 'info' and command != 'book' and command != 'read' and command != 'find':
+        print('\nОшибка:введена несуществующая команда\n')
+
+
+
